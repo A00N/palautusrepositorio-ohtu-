1716ae2d1d0338afe300c65060896b56d0f69f0a
Nooa Saareks <nsaareks@gmail.com>
2022-11-28 12:31:11 +0200

    Add more  tests

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -11,4 +11,24 @@ class TestOstoskori(unittest.TestCase):
         self.assertEqual(self.kori.hinta(), 0)
         self.assertEqual(self.kori.tavaroita_korissa(), 0)
 
+    def test_yhden_tuotteen_lisaaminen(self):
+        maito = Tuote("Maito", 2)
+        self.kori.lisaa_tuote(maito)
+
+        self.assertEqual(self.kori.tavaroita_korissa(), 1)
+
+    def test_ostoskorin_hinta_lisaamisen_jalkeen(self):
+        maito = Tuote("Maito", 2)
+        self.kori.lisaa_tuote(maito)
+
+        self.assertEqual(self.kori.hinta(), 2)
+
+    def test_kahden_tuotteen_lisaamisen_jalkeinen_maara_korissa(self):
+        maito = Tuote("Maito", 2)
+        limu = Tuote("Limu", 4)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(limu)
+
+        self.assertEqual(self.kori.tavaroita_korissa(), 2)
+
     
